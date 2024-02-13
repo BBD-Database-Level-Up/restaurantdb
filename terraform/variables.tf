f5c5655f462a89723484220e0a38d885b013a133
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-02-13 20:56:20 +0200

    only one variable ?

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,27 @@
-variable "aws_region" {
-  type = string
-}
-
 variable "username" {
   type = string
 }
 
-variable "password" {
-  type = string
-}
+# variable "aws_region" {
+#   type = string
+# }
 
-variable "aws_access_key" {
-  type = string
-}
+# variable "username" {
+#   type = string
+# }
 
-variable "aws_secret_key" {
-  type = string
-}
+# variable "password" {
+#   type = string
+# }
 
-variable "ingress_port" {
-  type = number
-}
+# variable "aws_access_key" {
+#   type = string
+# }
+
+# variable "aws_secret_key" {
+#   type = string
+# }
+
+# variable "ingress_port" {
+#   type = number
+# }
