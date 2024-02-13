13b13b9ddbfcfac006a979ef30d1e780644875cb
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-02-13 21:11:16 +0200

    Env vars should now work

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,23 @@
+variable "aws_region" {
+  type = string
+}
+
 variable "username" {
   type = string
 }
 
-# variable "aws_region" {
-#   type = string
-# }
+variable "password" {
+  type = string
+}
 
-# variable "username" {
-#   type = string
-# }
+variable "aws_access_key" {
+  type = string
+}
 
-# variable "password" {
-#   type = string
-# }
+variable "aws_secret_key" {
+  type = string
+}
 
-# variable "aws_access_key" {
-#   type = string
-# }
-
-# variable "aws_secret_key" {
-#   type = string
-# }
-
-# variable "ingress_port" {
-#   type = number
-# }
+variable "ingress_port" {
+  type = number
+}
