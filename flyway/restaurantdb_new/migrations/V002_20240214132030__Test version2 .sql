6318597005ad0f4a7383e3067bdf9bacf12a6692
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-02-14 15:26:08 +0200

    test change 2

---
+++ flyway/restaurantdb_new/migrations/V002_20240214132030__Test version2 .sql
@@ -0,0 +1,6 @@
+INSERT INTO Person (FirstName, LastName, DateOfBirth)
+VALUES ('John', 'Doe', '1990-05-15'),
+       ('Jane', 'Smith', '1985-09-20'),
+       ('Michael', 'Johnson', '1978-03-10'),
+       ('Emily', 'Brown', '1995-11-28'),
+       ('David', 'Wilson', '1980-07-03');
