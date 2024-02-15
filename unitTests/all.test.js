477aff2951854bbd67853adcb41ebddc86d3399e
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-02-15 16:15:10 +0200

    half finished unit tests

--- unitTests/all.test.js
+++ unitTests/all.test.js
@@ -1,15 +1,19 @@
 const sql = require('mssql');
-const USERNAME = 'test';
-const PASSWORD = 'test';
-const URL = 'restaurant-db.chulcebul0ir.eu-west-1.rds.amazonaws.com'
-
+const fs = require('fs');
+const USERNAME = 'mssql';
+const PASSWORD = 'Test12345@';
+const schemaPath = '../schema-model/Tables/schema.sql';
+const functionPaths = ['../schema-model/Functions/scalar-function.sql', '../schema-model/Functions/table-valued-function.sql']
 // Configure database connection
 const config = {
     user: USERNAME,
     password: PASSWORD,
-    server: 'sql-server',
     database: 'RestaurantDB',
-
+    server: "0.0.0.0",
+    options: {
+        enableArithAbort: true,
+        trustServerCertificate: true,
+    },
 };
 
 
@@ -17,14 +21,20 @@ const config = {
 beforeAll(async () => {
     try {
         await sql.connect(config);
+        const script = fs.readFileSync(schemaPath, 'utf8');
+        await sql.query(script);
+        functionPaths.forEach(await sql.query);
+        console.log('SQL script executed successfully.');
     } catch (err) {
-        console.error('Database connection error:', err);
+        console.error('Database setup error:', err);
     }
+
+
 });
 
 afterAll(async () => {
     try {
-        await sql.close();
+        await close();
     } catch (err) {
         console.error('Database close connection error:', err);
     }
@@ -34,7 +44,8 @@ afterAll(async () => {
 test('Average Rating for Menu Item Function', async () => {
     try {
         const menuItemId = 1; // Replace with the actual menu item ID
-        const result = await sql.query`SELECT dbo.fn_AverageRatingForMenuItem(${menuItemId}) AS AverageRating`;
+        await sql.connect(config);
+        const result = await sql.query(`SELECT dbo.fn_AverageRatingForMenuItem(${menuItemId}) AS AverageRating`);
         const averageRating = result.recordset[0].AverageRating;
 
         // Assert that the average rating is a decimal value
